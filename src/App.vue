71837bf83e307533c12934681cc8ec7981fb5f53
Manoel Shelby <manoelrocha.n93@gmail.com>
2022-05-08 19:25:27 -0300

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div>
+      <img src="" alt="" />
+    </div>
+    <nav></nav>
+  </header>
+  <RouterView />
+</template>
+
+<style></style>
---
+++ src/stores/pokemon.ts
@@ -0,0 +1,15 @@
+import { defineStore } from "pinia";
+
+export const usePokemonStore = defineStore("PokemonStore", {
+  state: () => ({
+    counter: 0,
+  }),
+  getters: {
+    doubleCount: (state) => state.counter * 2,
+  },
+  actions: {
+    increment() {
+      this.counter++;
+    },
+  },
+});
